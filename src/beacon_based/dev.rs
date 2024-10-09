ccbb97d0e089f35ced67e0aa4de5ed998e2e85ac
fh <fhfanhui@gmail.com>
2024-10-09 22:39:14 +0800

    write genesis after the generation

--- src/beacon_based/dev.rs
+++ src/beacon_based/dev.rs
@@ -703,13 +703,11 @@ where
                 ymlhdr["SECONDS_PER_ETH1_BLOCK"].as_u64().c(d!())?,
             ))
             .c(d!())?;
-
-            self.write_cfg().c(d!())?;
         }
 
         omit!(fs::remove_dir_all(&tmpdir));
 
-        Ok(())
+        self.write_cfg().c(d!())
     }
 
     // Apply genesis to node[s]:
